4f75d70c3b9f19a1100b3785bac1ae6f4db46fe0
marcosdiassobrinho <palmas0987@gmail.com>
2020-08-11 12:39:50 -0200

    m

---
+++ assets/logar.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#first-name').mask('000.000.000-00', {reverse: true});  
+  });
+
+$(document).ready(logar());
+  
+function logar(){
+    var usuario = document.querySelector('#first-name');
+    var senha = document.querySelector('#senha');
+    if (usuario.value == "123.123.123-12" && senha.value == "123") {
+        window.location.href = "login.html";        
+    } else{
+        alert("Usuário ou senha errada!");
+    }    
+};
